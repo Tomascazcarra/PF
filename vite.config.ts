4b1951b8c10d66e23bc3c4e32427bab307e9100d
Tomas Cazcarra <tomascazcarra@gmail.com>
2026-03-05 17:40:45 -0300

    si2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/PF/',   // ← ESTA LÍNEA ES LA IMPORTANTE
   server: {
     port: 3000,
     host: '0.0.0.0',
